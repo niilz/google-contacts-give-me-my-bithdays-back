0839a531c1ebd6f4751074b3c355dfea7ceb0ce7
niilz <niilz@outlook.de>
2025-09-26 16:15:04 +0300

    Add basic server setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+use std::net::SocketAddr;
+use std::str::FromStr;
+
+use warp::{Filter, serve};
+
+#[tokio::main]
+async fn main() {
+    let hi = warp::path("code")
+        .and(warp::path::param())
+        //.and(warp::header("???"))
+        .map(|param: String| format!("Param: {param}"));
+    let server = serve(hi);
+    server
+        .run(SocketAddr::from_str("127.0.0.1:5000").expect("no valid socket-addr"))
+        .await;
     println!("Hello, world!");
 }
