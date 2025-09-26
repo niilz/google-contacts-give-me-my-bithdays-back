fdb45e792baa002eff3058cba7e9e9ef07e874fb
niilz <niilz@outlook.de>
2025-09-26 16:37:50 +0300

    change path to query

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,20 @@
+use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::str::FromStr;
 
+use warp::reply::Response;
 use warp::{Filter, serve};
 
 #[tokio::main]
 async fn main() {
     let hi = warp::path("code")
-        .and(warp::path::param())
-        //.and(warp::header("???"))
-        .map(|param: String| format!("Param: {param}"));
+        .and(warp::query::<HashMap<String, String>>())
+        .map(|map| {
+            for (key, value) in map {
+                println!("key: {key}, value: {value}");
+            }
+            Response::new("foo".into())
+        });
     let server = serve(hi);
     server
         .run(SocketAddr::from_str("127.0.0.1:5000").expect("no valid socket-addr"))
