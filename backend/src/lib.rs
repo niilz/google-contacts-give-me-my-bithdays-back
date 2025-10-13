2edfdd8f68c7a0dd45da2f6eb4f67e2dfcf9e772
niilz <niilz@outlook.de>
2025-10-13 21:56:38 +0200

    add response structure

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -9,3 +9,46 @@ pub struct AuthTokens {
     pub token_type: String,
     pub refresh_token_expires_in: Option<u32>,
 }
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+pub struct Birthdays {
+    connections: Vec<Connection>,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+pub struct Connection {
+    #[serde(rename = "resourceName")]
+    resource_name: String,
+    etag: String,
+    names: Vec<ConnectionData>,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+struct ConnectionData {
+    metadata: MetaData,
+    #[serde(rename = "displayName")]
+    display_name: String,
+    #[serde(rename = "familyName")]
+    family_name: String,
+    #[serde(rename = "givenName")]
+    given_name: String,
+    #[serde(rename = "displayNameLastFirst")]
+    display_name_last_first: String,
+    #[serde(rename = "unstructuredName")]
+    unstructured_name: String,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+struct MetaData {
+    primary: bool,
+    source: Source,
+    #[serde(rename = "sourcePrimary")]
+    source_primary: bool,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+struct Source {
+    #[serde(rename = "sourcePrimary")]
+    typ: String,
+    id: String,
+}
